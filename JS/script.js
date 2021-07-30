15b270220f5e78adbb08987417ff370c09892a1c
KyleTrippk <kyle.kibet@student.moringaschool.com>
2021-07-31 01:28:38 +0300

    adds link to the custom JS, the external CDN JS and the external bootstrap

---
+++ JS/script.js
@@ -0,0 +1,12 @@
+// user interface
+$(document).ready(function(){
+    $("#development").click(function(){$("#development-hide").toggle(500)});
+    $("#aboutPizza").addClass("text-center");
+    $("#pizzacrust").addClass("text-center");
+    $("#location").addClass("text-center");
+    $("#toppings").addClass("text-center");
+    $("#toppingsbuttons").addClass("text-center");
+    $("#locationsection").addClass("text-center");
+    $("#idsects").addClass("text-center");
+    $("#lastly").addClass("text-center");
+})

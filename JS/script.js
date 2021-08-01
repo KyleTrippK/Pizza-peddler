970bc3b873a228161b6ad8b0c80b423d234d8196
KyleTrippk <kyle.kibet@student.moringaschool.com>
2021-08-01 10:57:22 +0300

    adds a JS condition to validate the main form

--- JS/script.js
+++ JS/script.js
@@ -8,5 +8,67 @@ $(document).ready(function(){
     $("#toppingsbuttons").addClass("text-center");
     $("#locationsection").addClass("text-center");
     $("#idsects").addClass("text-center");
-    $("#lastly").addClass("text-center");
+    $("#final").addClass("text-center");
+    $("#jaribio").hide();
+})
+
+// Business logic
+var sizePrice, crustPrice, toppingPrice, pizzaPrice;
+function pizza(size, crust, toppings, number){
+    this.size = size;
+    this.crust = crust;
+    this.toppings = toppings;
+    this.number = number;
+}
+$(document).ready(function(){
+    $("#choiceForm").submit(function(event){
+        event.preventDefault();
+        var pizzaSize = $("#pizzaSize").val();
+        var pizzaCrust = $("#pizzaCrust").val();
+        var pizzaTopping = $("#pizzaTopping").val();
+        var pizzaNumber = $("#pizzaNumber").val();
+        var piza = new pizza(pizzaSize, pizzaCrust, pizzaTopping, pizzaNumber);
+        //pizza size against price
+        if(piza.size == "small"){
+            sizePrice = 500;
+        }
+        else if(piza.size == "medium"){
+            sizePrice = 800;
+        }
+        else{
+            sizePrice = 1000;
+        }
+        //pizza crust against price
+        if(piza.crust == "stuffed crust"){
+            crustPrice = 50;
+        }
+        else if(piza.crust == "pizza bagels"){
+            crustPrice = 40;
+        }
+        else if(piza.crust == "flat bread"){
+            crustPrice = 60;
+        }
+        else if(piza.crust == "chicago deep dish"){
+            crustPrice = 80;
+        }
+        else{
+            crustPrice = 70;
+        }
+        //pizza toppings against price
+        if(piza.toppings == "fresh basil"){
+            toppingPrice = 10;
+        }
+        else if(piza.toppings == "black olives"){
+            toppingPrice = 20;
+        }
+        else if(piza.toppings == "pineapple"){
+            toppingPrice = 30;
+        }
+        else{
+            toppingPrice = 40;
+        }
+        pizzaPrice = sizePrice + crustPrice + toppingPrice;
+        $("#jaribio").show();
+        $("#showprice").append(pizzaPrice);
+    })
 })
